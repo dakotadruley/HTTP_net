ae9b24de2b11d53aa01f141eaaed8b2e678faa6b
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-16 16:51:28 -0700

    jose helped me get the test to pass for the createRespone function

--- test/app.test.js
+++ test/app.test.js
@@ -2,5 +2,43 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+  it('gets red html page', () => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual(`
+            <html>
+              <body>
+                <h1>This is red</h1>
+              </body>
+            </html>
+            `);
+      });
+  });
+  it('gets blue html page', () => {
+    return request(app)
+      .get('/blue')
+      .then(res => {
+        expect(res.text).toEqual(`
+            <html>
+              <body>
+                <h1>This is red</h1>
+              </body>
+            </html>
+            `);
+      });
+  });
+  it('gets green html page', () => {
+    return request(app)
+      .get('/green')
+      .then(res => {
+        expect(res.text).toEqual(`
+            <html>
+              <body>
+                <h1>This is red</h1>
+              </body>
+            </html>
+            `);
+      });
+  });
 });
